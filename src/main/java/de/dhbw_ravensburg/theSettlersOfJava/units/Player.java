adeafa68670441b397111e08c980bde1de0114a0
Jonas Thelen <jonas.thelen@icloud.com>
2025-05-13 11:55:18 +0200

    add: player class

--- src/main/java/de/dhbw_ravensburg/theSettlersOfJava/units/Player.java
+++ src/main/java/de/dhbw_ravensburg/theSettlersOfJava/units/Player.java
@@ -1,5 +1,25 @@
 package de.dhbw_ravensburg.theSettlersOfJava.units;
 
 public class Player {
+	private static int ID_COUNTER = 0;
+	private int id;
+	private int victoryPoints;
+	
+	public Player() {
+		this.id = ID_COUNTER;
+		this.victoryPoints = 0;
+		ID_COUNTER++;
+	}
 
+	public int getVictoryPoints() {
+		return victoryPoints;
+	}
+
+	public void addVictoryPoints(int differenz) {
+		this.victoryPoints += differenz;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
