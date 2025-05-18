8155848e99819d2e66fc85d5f9384bb5a6b68a16
Jonas Thelen <jonas.thelen@icloud.com>
2025-05-19 00:01:58 +0200

    Change background image code to work

--- src/main/java/de/dhbw_ravensburg/theSettlersOfJava/CatanMainMenu.java
+++ src/main/java/de/dhbw_ravensburg/theSettlersOfJava/CatanMainMenu.java
@@ -16,31 +16,34 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
+import java.io.InputStream;
+
 public class CatanMainMenu extends FXGLMenu {
-    
+
     private static final int BUTTON_WIDTH = 200;
     private static final int BUTTON_HEIGHT = 60;
-    
+
     public CatanMainMenu() {
         super(MenuType.MAIN_MENU);
+
+        // Background image
+        String backgroundImagePath = "/images/startscreen.png";
+        InputStream imageStream = getClass().getResourceAsStream(backgroundImagePath);
         
-        // Füge Hintergrundbild hinzu
-      //Bilddatei ist irgendwie fehlerhaft, entweder alternatives Bild oder reparieren
-        String backgroundImagePath = "/images/startscreen.png"; // Pfad zum Hintergrundbild anpassen
-        try {
-            Image backgroundImage = new Image(getClass().getResourceAsStream(backgroundImagePath));
+        if (imageStream != null) {
+            Image backgroundImage = new Image(imageStream);
             ImageView backgroundView = new ImageView(backgroundImage);
-            backgroundView.setFitWidth(FXGL.getAppWidth()+500); //Muss noch vielleicht angepasst werden!!
+            backgroundView.setFitWidth(FXGL.getAppWidth());
             backgroundView.setFitHeight(FXGL.getAppHeight());
             getContentRoot().getChildren().add(backgroundView);
-        } catch (Exception e) {
-            // Fallback bei Fehlern: Einfarbiger Hintergrund
+        } else {
+            // Fallback solid color
             Rectangle background = new Rectangle(FXGL.getAppWidth(), FXGL.getAppHeight());
             background.setFill(Color.DARKGREEN);
             getContentRoot().getChildren().add(background);
         }
-        
-        // Titel erstellen
+
+        // Title
         Text title = new Text("The Settlers of Java");
         title.setFont(Font.font("Arial", 48));
         title.setFill(Color.WHITE);
@@ -48,69 +51,51 @@ public class CatanMainMenu extends FXGLMenu {
         title.setStrokeWidth(2);
         title.setTextAlignment(TextAlignment.CENTER);
         title.setEffect(new DropShadow(10, Color.BLACK));
-        
-        // Buttons erstellen
+
+        // Buttons
         Button startButton = createButton("Spiel starten");
         startButton.setOnAction(e -> fireNewGame());
-        
+
         Button optionsButton = createButton("Optionen");
         optionsButton.setOnAction(e -> {
-            // Hier später Optionen-Funktionalität einfügen
+            // Optionen-Funktionalität hier einfügen
         });
-        
+
         Button exitButton = createButton("Spiel beenden");
         exitButton.setOnAction(e -> fireExit());
-        
-        // Layout erstellen
-        VBox menuBox = new VBox(20);
+
+        // Layout
+        VBox menuBox = new VBox(20, title, startButton, optionsButton, exitButton);
         menuBox.setAlignment(Pos.CENTER);
-        menuBox.getChildren().addAll(title, startButton, optionsButton, exitButton);
-        
-        getContentRoot().getChildren().add(menuBox);
-        
-        // Zentriere das Menü
-        menuBox.setTranslateX(FXGL.getAppWidth() / 2 - BUTTON_WIDTH / 2);
+        menuBox.setTranslateX(FXGL.getAppWidth() / 2 - BUTTON_WIDTH);
         menuBox.setTranslateY(FXGL.getAppHeight() / 2 - 150);
+
+        getContentRoot().getChildren().add(menuBox);
     }
-    
+
     private Button createButton(String text) {
         Button button = new Button(text);
-        button.setPrefWidth(BUTTON_WIDTH);
-        button.setPrefHeight(BUTTON_HEIGHT);
+        button.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         button.setFont(Font.font("Arial", 20));
-        
-        // Button-Styling
-        button.setStyle(
-            "-fx-background-color: #f5f5dc; " + // beige Farbe
-            "-fx-background-radius: 15; " + // abgerundete Ecken
-            "-fx-border-color: #a57e38; " + // dunklere Umrandung
-            "-fx-border-width: 2; " +
-            "-fx-border-radius: 15;"
-        );
-        
-        // Hover-Effekt
-        button.setOnMouseEntered(e -> {
-            button.setStyle(
-                "-fx-background-color: #a57e38; " + // dunklere Farbe beim Hover
-                "-fx-background-radius: 15; " +
-                "-fx-border-color: #8b5c27; " +
-                "-fx-border-width: 2; " +
-                "-fx-border-radius: 15; " +
-                "-fx-text-fill: white;"
-            );
-        });
-        
-        button.setOnMouseExited(e -> {
-            button.setStyle(
-                "-fx-background-color: #f5f5dc; " +
-                "-fx-background-radius: 15; " +
-                "-fx-border-color: #a57e38; " +
-                "-fx-border-width: 2; " +
-                "-fx-border-radius: 15; " +
-                "-fx-text-fill: black;"
-            );
-        });
-        
+
+        String baseStyle = "-fx-background-color: #f5f5dc; " +
+                           "-fx-background-radius: 15; " +
+                           "-fx-border-color: #a57e38; " +
+                           "-fx-border-width: 2; " +
+                           "-fx-border-radius: 15; " +
+                           "-fx-text-fill: black;";
+        String hoverStyle = "-fx-background-color: #a57e38; " +
+                            "-fx-background-radius: 15; " +
+                            "-fx-border-color: #8b5c27; " +
+                            "-fx-border-width: 2; " +
+                            "-fx-border-radius: 15; " +
+                            "-fx-text-fill: white;";
+
+        button.setStyle(baseStyle);
+
+        button.setOnMouseEntered(e -> button.setStyle(hoverStyle));
+        button.setOnMouseExited(e -> button.setStyle(baseStyle));
+
         return button;
     }
 }
