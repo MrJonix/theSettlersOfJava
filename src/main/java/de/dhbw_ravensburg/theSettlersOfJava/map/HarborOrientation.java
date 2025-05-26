ddea33d0e15e391e7ec1193d9853b4d844ba631f
Jonas Thelen <jonas.thelen@icloud.com>
2025-05-26 20:18:29 +0200

    Refactor habor

---
+++ src/main/java/de/dhbw_ravensburg/theSettlersOfJava/map/HarborOrientation.java
@@ -0,0 +1,11 @@
+package de.dhbw_ravensburg.theSettlersOfJava.map;
+
+public enum HarborOrientation {
+    TOP_LEFT,
+    TOP_RIGHT,
+    MIDDLE_LEFT,
+    MIDDLE_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT;
+}
+
---
+++ src/main/java/de/dhbw_ravensburg/theSettlersOfJava/resources/HarborType.java
@@ -0,0 +1,10 @@
+package de.dhbw_ravensburg.theSettlersOfJava.resources;
+
+public enum HarborType {
+    BRICK,
+    WOOD,
+    WOOL,
+    WHEAT,
+    ORE,
+    THREE_TO_ONE
+}
